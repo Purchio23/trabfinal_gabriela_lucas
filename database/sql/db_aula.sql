d91431cde63409008e318519d93ba9a4a9b41a60
lucas <lucassilvapurchio11@gmail.com>
2023-06-02 15:44:05 -0300

    banco

--- database/sql/db_aula.sql
+++ database/sql/db_aula.sql
@@ -1,30 +1,199 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
+-- --------------------------------------------------------
 
---
--- Estrutura da tabela `usuario`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+
+-- Copiando estrutura do banco de dados para db_pweb2_2023_1
+CREATE DATABASE IF NOT EXISTS `db_pweb2_2023_1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_pweb2_2023_1`;
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.categoria
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.categoria: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.failed_jobs: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.leitura
+CREATE TABLE IF NOT EXISTS `leitura` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `data_leitura` date NOT NULL,
+  `hora_leitura` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `valor_sensor` double(8,2) NOT NULL,
+  `sensor_id` bigint unsigned DEFAULT NULL,
+  `mac_id` bigint unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `leitura_sensor_id_foreign` (`sensor_id`),
+  KEY `leitura_mac_id_foreign` (`mac_id`),
+  CONSTRAINT `leitura_mac_id_foreign` FOREIGN KEY (`mac_id`) REFERENCES `mac` (`id`),
+  CONSTRAINT `leitura_sensor_id_foreign` FOREIGN KEY (`sensor_id`) REFERENCES `sensor` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.leitura: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.mac
+CREATE TABLE IF NOT EXISTS `mac` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contador` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.mac: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.migrations: ~11 rows (aproximadamente)
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_resets_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+	(5, '2023_04_14_165129_create_usuario', 1),
+	(6, '2023_04_28_175149_create_categorias_table', 1),
+	(7, '2023_05_12_170844_mac', 1),
+	(8, '2023_05_12_170845_sensor', 1),
+	(9, '2023_05_12_170856_leitura', 1),
+	(10, '2023_06_02_172652_create_servico1_table', 2),
+	(11, '2023_06_02_173022_create_servico1_table]', 2);
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.password_resets: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.personal_access_tokens: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.sensor
+CREATE TABLE IF NOT EXISTS `sensor` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contador` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.sensor: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.servico1
+CREATE TABLE IF NOT EXISTS `servico1` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
   `telefone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL)
-  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `categoria_id` bigint unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoria_id` (`categoria_id`),
+  CONSTRAINT `servico1_categoria_id_foreign` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-  CREATE TABLE `servico1` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+-- Copiando dados para a tabela db_pweb2_2023_1.servico1: ~1 rows (aproximadamente)
+INSERT INTO `servico1` (`id`, `nome`, `telefone`, `email`, `created_at`, `updated_at`, `categoria_id`) VALUES
+	(1, 'ccccc', '888888888888', 'c@c.com', '2023-06-02 21:19:43', '2023-06-02 21:19:43', NULL);
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela db_pweb2_2023_1.users: ~2 rows (aproximadamente)
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'aaa', 'a@a.com', NULL, '$2y$10$B7ixEgZdcrZKp9GPuAA57usN4sLjPsOsrGnnIKcHq23k9IhdlWsS.', NULL, '2023-06-02 20:02:48', '2023-06-02 20:02:48'),
+	(2, 'bbb', 'b@b.com', NULL, '$2y$10$4jevc9J1AM8306pHu59rWOmbATfP1kB.cr1bcTVEVOqFaIexYGcGi', NULL, '2023-06-02 20:36:45', '2023-06-02 20:36:45');
+
+-- Copiando estrutura para tabela db_pweb2_2023_1.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
   `telefone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagem` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL)
-  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `categoria_id` bigint unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_categoria_id_foreign` (`categoria_id`),
+  CONSTRAINT `usuario_categoria_id_foreign` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Extraindo dados da tabela `usuario`
---
+-- Copiando dados para a tabela db_pweb2_2023_1.usuario: ~0 rows (aproximadamente)
 
-INSERT INTO `usuario` (`id`, `nome`, `telefone`, `email`, `created_at`, `updated_at`) VALUES
-(1, 'Jackson', '84 98866-5500', 'lordjackson@gmail.com', '2023-04-14 20:15:00', '2023-04-14 20:15:00'),
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
